a162b9cd83c85e58076cb008477950246418feaf
SentinelReacon <84021424+SentinelReacon@users.noreply.github.com>
2023-10-15 18:36:15 +0530

    Format input from terminal (#10)
    * Add methods in the main file
    
    * Format the terminal input

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ while True:
     print("3. Mark Task as Completed")
     print("4. Quit")
     
-    usr_input = int(input("Enter your choice"))
+    usr_input = int(input("Enter your choice: "))
 
     if usr_input == 1:
-        task_input = input("Enter the task details")
+
+        task_input = input("Enter the task details: ")
+
         todo.add_task(task_input)
     
     elif usr_input == 2:
         todo.list_task()
 
     elif usr_input == 3:
-        i = int(input("Enter task id"))
+
+        i = int(input("Enter task id: "))
+
         todo.delete_task(i)
 
     elif usr_input == 4:

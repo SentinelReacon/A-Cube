dd462007d4f38e39b099b416eb75cce7fb8c32c2
Aakash Thatte <aathatte2002@gmail.com>
2023-10-15 18:43:01 +0530

    Fix conflicts

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
 from task import ToDoList
+from colorama import Fore, Back, Style
 
 todo = ToDoList()
 
 while True:
-    print("\nTodo List Menu:")
-    print("1. Add Task")
-    print("2. List Tasks")
-    print("3. Mark Task as Completed")
-    print("4. Quit")
+    print(Fore.WHITE,"\nTodo List Menu:")
+    print(Fore.BLUE, "1. Add Task")
+    print(Fore.BLUE, "2. List Tasks")
+    print(Fore.BLUE, "3. Mark Task as Completed")
+    print(Fore.BLUE, "4. Quit")
     
-    usr_input = int(input("Enter your choice: "))
+    usr_input = int(input(Fore.RED + "Enter your choice:"))
 
     if usr_input == 1:
-
-        task_input = input("Enter the task details: ")
-
+        task_input = input(Fore.RED + "Enter the task details:")
         todo.add_task(task_input)
     
     elif usr_input == 2:
         todo.list_task()
 
     elif usr_input == 3:
-
-        i = int(input("Enter task id: "))
-
+        i = int(input(Fore.RED + "Enter task id:"))
         todo.delete_task(i)
 
     elif usr_input == 4:

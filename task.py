9832e52ae49652ef74e3409ee9588fa1b8eeb9a5
Amogh Joshi <amoghjoshi500@gmail.com>
2023-10-15 17:15:03 +0530

    Add the list_task method in the code

--- task.py
+++ task.py
@@ -11,3 +11,8 @@ class ToDoList:
 
     def mark_complete(self, task_id):
         pass
+
+    def list_task():
+        for i, task in enumerate(tasks, start=1):
+            status = "Done" if task["completed"] else "Not Done"
+            print(f"{i}. Title: {task['title']}, Description: {task['description']}, Status: {status}")

17b1a8f6f8f3cc20f3b62a02ed0ea8df9f99a2fb
Amogh Joshi <amoghjoshi500@gmail.com>
2023-10-15 18:28:16 +0530

    Add methods in the main file

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ while True:
     if usr_input == 1:
         task_input = input("Enter the task details")
         todo.add_task(task_input)
+    
+    elif usr_input == 2:
+        todo.list_task()
+
+    elif usr_input == 3:
+        i = int(input("Enter task id"))
+        todo.delete_task(i)
+
+    elif usr_input == 4:
+        break

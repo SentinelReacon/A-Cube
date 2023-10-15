3fce3a0e3d910a37c2fbb36bff19a01bac44a79c
VishnoiAman777 <amanvishnoi777@gmail.com>
2023-10-15 16:49:26 +0530

    Added Basic Structure

---
+++ main.py
@@ -0,0 +1,9 @@
+from task import ToDoList
+
+while True:
+    print("\nTodo List Menu:")
+    print("1. Add Task")
+    print("2. List Tasks")
+    print("3. Mark Task as Completed")
+    print("4. Quit")
+    pass

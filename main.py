11351e7bce5419106b9672906da800d41b4c104e
Aakash Thatte <aathatte2002@gmail.com>
2023-10-15 17:27:08 +0530

    Add code for user option to display list

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from task import ToDoList
 
+todolist = ToDoList()
 while True:
     print("\nTodo List Menu:")
     print("1. Add Task")
     print("2. List Tasks")
     print("3. Mark Task as Completed")
     print("4. Quit")
+
+    user_input = int(input("Enter option"))
+    if user_input==2:
+        todolist.list_task()
     pass

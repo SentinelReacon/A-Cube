6726ca04da9251982f351e147697bb60422c975c
Aakash Thatte <aathatte2002@gmail.com>
2023-10-16 10:37:37 +0530

    Add docs for todolist to show cherry pick

--- task.py
+++ task.py
@@ -1,10 +1,13 @@
 from colorama import Fore, Back, Style
 class ToDoList:
+    # This is a todo list class
+    # all methods related to the list will be in hre
     def __init__(self) -> None:
         self.task_details = {} # task_id: task_name pair
         # self.task_completed = {} # When completed it remove task from task details add it to task completed
 
     def add_task(self, task):
+        # This function adds a task to the todo list
         key = len(self.task_details)
         self.task_details[key+1] = task
         print("Task added Successfully")
@@ -18,6 +21,7 @@ class ToDoList:
         print(Fore.YELLOW + "Task deleted Successfully")
 
     def list_task(self):
+        # This function lists all tasks in the todo list
         print(Fore.LIGHTGREEN_EX + "Task details")
         for i, task in self.task_details.items():
             print(Fore.GREEN + f"{i}. Title: {task}")
